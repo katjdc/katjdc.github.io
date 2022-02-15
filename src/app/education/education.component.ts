e5bf79a3503b840c1604f5e852da47e4583eecea
Katrine Joy Dela Cruz <kdelacruz.ivyleague@gmail.com>
2022-02-16 04:52:25 +0800

    update

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+// ./education.component.html
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
@@ -8,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class EducationComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
 }
